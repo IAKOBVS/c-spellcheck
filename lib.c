d6f2b21385f20e072b53b65e2fe0b64a4c33252a
James Tirta Halim <tirtajames45@gmail.com>
2024-07-24 16:25:43 +0700

    update: c-spellcheck

--- lib.c
+++ lib.c
@@ -357,3 +357,35 @@ cs_suggest_header_to_include(const char *fn, int fn_len, const char *dir)
 	(void)dir;
 	return NULL;
 }
+
+fns_ty *
+autosuggest(fns_ty *cal_head, fns_ty *decl_head, jtrie_node_ty *trie_head, const char *file)
+{
+	cs_fns_read_from_buffer(decl_head, cal_head, trie_head, file);
+	fns_ty *cal_node;
+	for (cal_node = cal_head; cal_node; cal_node = cal_node->next)
+		if (cal_node->value) {
+			/* Check trie for exact match. If a match is found,
+			 * either the called function is declared or it has
+			 * been checked. */
+			if (!jtrie_match(trie_head, cal_node->value)) {
+				int lev;
+				fns_ty *similar = cs_fns_get_most_similar_string(decl_head, cal_node->value, LEV_MAX(strlen(cal_node->value)), &lev);
+				if (similar) {
+					if (lev > 0)
+						printf("\"%s\" is an undeclared function. Did you mean \"%s\"?\n", cal_node->value, similar->value);
+				} else {
+					const char *header = cs_suggest_header_to_include(cal_node->value, strlen(cal_node->value), "");
+					if (header) {
+						printf("\"%s\" is an undeclared function. Did you mean to include \"%s\"?\n", cal_node->value, header);
+					} else {
+						printf("\"%s\" is an undeclared function.\n", cal_node->value);
+					}
+				}
+				/* Add called function to trie so multiple occurences of
+				 * the same called functions will only be checked once. */
+				jtrie_add(trie_head, cal_node->value);
+			}
+		}
+	return 0;
+}
--- main.c
+++ main.c
@@ -6,42 +6,17 @@ int
 main(int argc, char **argv)
 {
 	assert(!argv[1] || argv[1][0]);
-	char *s = cs_file_read_alloc(argv[1]);
+
+	char *file = cs_file_read_alloc(argv[1]);
 	jtrie_node_ty *trie_head = jtrie_init();
 	fns_ty *decl_head = cs_fns_alloc(), *cal_head = cs_fns_alloc();
-	fns_ty *decl_node, *cal_node;
-	cs_fns_read_from_buffer(decl_head, cal_head, trie_head, s);
 
-	(void)decl_node;
+	autosuggest(cal_head, decl_head, trie_head, file);
 
-	for (cal_node = cal_head; cal_node; cal_node = cal_node->next)
-		if (cal_node->value) {
-			/* Check trie for exact match. If a match is found,
-			 * either the called function is declared or it has
-			 * been checked. */
-			if (!jtrie_match(trie_head, cal_node->value)) {
-				int lev;
-				fns_ty *similar = cs_fns_get_most_similar_string(decl_head, cal_node->value, LEV_MAX(strlen(cal_node->value)), &lev);
-				if (similar) {
-					if (lev > 0)
-						printf("\"%s\" is an undeclared function. Did you mean \"%s\"?\n", cal_node->value, similar->value);
-				} else {
-					const char *header = cs_suggest_header_to_include(cal_node->value, strlen(cal_node->value), "");
-					if (header) {
-						printf("\"%s\" is an undeclared function. Did you mean to include \"%s\"?\n", cal_node->value, header);
-					} else {
-						printf("\"%s\" is an undeclared function.\n", cal_node->value);
-					}
-				}
-				/* Add called function to trie so multiple occurences of
-				 * the same called functions will only be checked once. */
-				jtrie_add(trie_head, cal_node->value);
-			}
-		}
-
-	cs_file_read_free(s);
+	cs_file_read_free(file);
 	cs_fns_freeall(decl_head);
 	cs_fns_freeall(cal_head);
 	jtrie_free(&trie_head);
+
 	return 0;
 }
