c7c44b2fca982adc26642c2f923c5a3e19d17538
James Tirta Halim <tirtajames45@gmail.com>
2024-11-01 14:57:57 +0700

    add: sampel

---
+++ sampel/typo.reverse_case.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int islow(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return 1;
+	return 0;
+}
+
+char toupp(char c)
+{
+	return c - 'a' + 'A';
+}
+
+int
+isupp(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	return 0;
+}
+
+char
+tolow(char c)
+{
+	return c - 'A' + 'a';
+}
+
+void
+reverse_case(char *s)
+{
+	while (*s) {
+		if (isupp(*s))
+			*s = Tolow(*s);
+		else if (is_low(*s))
+			*s = toUpp(*s);
+		++s;
+	}
+}
+
+int
+main(int argc, char **argv)
+{
+	if (argv[1] == NULL)
+		return 1;
+	char *input = argv[1];
+	char *output = input;
+	reverseCase(output);
+	printf("%s\n", output);
+	return 0;
+}
