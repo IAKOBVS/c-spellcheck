a8b688d6d802aef018315c9d53636326bbf8952c
James Tirta Halim <tirtajames45@gmail.com>
2024-11-01 14:50:39 +0700

    add: sampel

---
+++ sampel/is_eq_ignore_case.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int isupp(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	return 0;
+}
+
+char tolow(char c)
+{
+	return c - 'A' + 'a';
+}
+
+char norm(char c)
+{
+	if (isupp(c))
+		return tolow(c);
+	return c;
+}
+
+size_t
+is_eq_icase(char *s, char *t)
+{
+	while (*s) {
+		if (norm(*s) != norm(*t))
+			return 0;
+		++s;
+		++t;
+	}
+	return 1;
+}
+
+int
+main(int argc, char **argv)
+{
+	if (argv[1] == NULL || argv[2] == NULL)
+		return 1;
+	char *input1 = argv[1];
+	char *input2 = argv[2];
+	printf("%s\n", is_eq_icase(input1, input2) ? "true" : "false");
+	return 0;
+}
